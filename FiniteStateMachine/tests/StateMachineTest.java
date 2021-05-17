f16731309096a1c4ebabc8f7317337d256ad614c
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-17 22:57:13 +0200

    Tests toegeveogd, test voor getnodes gemaakt.

--- FiniteStateMachine/tests/StateMachineTest.java
+++ FiniteStateMachine/tests/StateMachineTest.java
@@ -1,8 +1,28 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StateMachineTest {
+    Node node = new Node("node");
+    Node node1 = new Node("anothernode");
+    Node node2 = new Node("newnode");
+    Node node3 = new Node("subnode");
+    StateMachine State = new StateMachine();
+
+    @Test
+    void getNodes() {
+        State.setNodes(Stream.of(node3, node2, node).collect(Collectors.toCollection(HashSet::new)));
+        assertEquals(Stream.of(node3, node2, node).collect(Collectors.toCollection(HashSet::new)),
+                State.getNodes());
+    }
+
+    @Test
+    void getCurrentNode() {
+    }
 
     @Test
     void startText() {
