b51f66477604f68863c85510a82a16f3c67235be
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-17 22:40:25 +0200

    Tests gemaakt voor node class.

--- FiniteStateMachine/tests/NodeTest.java
+++ FiniteStateMachine/tests/NodeTest.java
@@ -1,18 +1,29 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NodeTest {
+    private Node node = new Node("node");
+    private Node node1 = new Node("anothernode");
+    private Node node2 = new Node("newnode");
+    private Node node3 = new Node("subnode");
 
     @Test
-    void getNaam() {
+    void getNaam() throws Exception {
+        assertEquals("node", node.getNaam());
     }
 
     @Test
-    void getOvergangen() {
-    }
-
-    @Test
-    void setOvergangen() {
+    void getOvergangen() throws Exception{
+        node.setOvergangen(new HashMap<>() {{
+            put("A", node2);
+            put("B", node1);
+        }});
+        assertEquals(new HashMap<>() {{
+            put("A", node2);
+            put("B", node1);
+        }}, node.getOvergangen());
     }
 }
