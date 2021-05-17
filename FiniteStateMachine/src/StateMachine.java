a69fa6693819e87962b58a0f2699591d9718be2a
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-17 21:12:31 +0200

    De textmethode gemaakt.

--- FiniteStateMachine/src/StateMachine.java
+++ FiniteStateMachine/src/StateMachine.java
@@ -3,14 +3,30 @@ import java.util.*;
 
 public class StateMachine {
     private HashSet<Node> Nodes;
-    private Node StartNode;
+    private Node CurrentNode;
 
     public StateMachine(HashSet Nodes, Node StartNode) {
         this.Nodes = Nodes;
-        this.StartNode = StartNode;
+        this.CurrentNode = StartNode;
     }
 
-    public void StartText(){}
+    public void StartText(){
+        while (true) {
+            System.out.println("Huidige node: " + CurrentNode.getNaam());
+            for (String key : CurrentNode.getOvergangen().keySet()) {
+                System.out.println(key + ": " + CurrentNode.getOvergangen().get(key).getNaam());
+            }
+            Scanner scan = new Scanner(System.in);
+            String input = scan.next();
+            if (CurrentNode.getOvergangen().containsKey(input)) {
+                CurrentNode = CurrentNode.getOvergangen().get(input);
+            }
+            else {
+                System.out.println("Fout: dit is geen mogelijke overgang");
+                return;
+            }
+        }
+    }
 
     public void StartRandom(){}
 }
