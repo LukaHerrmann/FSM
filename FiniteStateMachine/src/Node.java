3b3d5f781e579458db9440fffcfbd8d361c460a2
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-17 21:13:36 +0200

    De Node class aangevuld met getters en setters. Ook Hashset naar hashmap veranderd.

--- FiniteStateMachine/src/Node.java
+++ FiniteStateMachine/src/Node.java
@@ -1,6 +1,27 @@
 
 import java.util.*;
 
-public class Node {
-    Set<Node> Overgangen = new HashSet<Node>();
+public class Node{
+    private String naam;
+    private HashMap<String, Node> Overgangen;
+
+
+    public Node (String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+
+    public HashMap<String, Node> getOvergangen() {
+        return Overgangen;
+    }
+
+    public void setOvergangen(HashMap<String, Node> overgangen) {
+        Overgangen = overgangen;
+    }
+
+
 }
