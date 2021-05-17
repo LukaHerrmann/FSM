1064adc778db8536ac4b86e8f1126388ec67dfee
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-17 18:54:33 +0200

    default package voor Statemachine

--- FiniteStateMachine/src/StateMachine.java
+++ FiniteStateMachine/src/StateMachine.java
@@ -1,2 +1,13 @@
+
+import java.util.*;
+
 public class StateMachine {
+    private Set<Node> Nodes;
+    private Node StartNode;
+
+    public StateMachine(HashSet Nodes, Node StartNode) {
+        this.Nodes = Nodes;
+        this.StartNode = StartNode;
+    }
+
 }
