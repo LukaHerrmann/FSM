854cf728e7289c97f86cde6610d7cc22b530b49f
LukaHerrmann <lukajherrmann@gmail.com>
2021-05-17 22:30:22 +0200

    Tests gemaakt voor de classes.

---
+++ FiniteStateMachine/tests/NodeTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void getNaam() {
+    }
+
+    @Test
+    void getOvergangen() {
+    }
+
+    @Test
+    void setOvergangen() {
+    }
+}
